304d7084e82d9430ecd329afd94687042990df42
nhkim <nhkim1529@gmail.com>
2024-01-26 11:28:50 +0900

    Init: 초기설정

--- Ex10_2_Room/app/src/main/java/com/nhkim/ex10_2_room/MainActivity.kt
+++ Ex10_2_Room/app/src/main/java/com/nhkim/ex10_2_room/MainActivity.kt
@@ -2,10 +2,89 @@ package com.nhkim.ex10_2_room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import com.nhkim.ex10_2_room.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var myDao: MyDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        myDao = MyDatabase.getDatabase(this).getMyDao()
+
+        val allStudents = myDao.getAllStudents()
+        allStudents.observe(this) {
+            val str = StringBuilder().apply {
+                for ((id, name) in it) {
+                    append(id)
+                    append("-")
+                    append(name)
+                    append("\n")
+                }
+            }.toString()
+            binding.textStudentList.text = str
+        }
+
+        binding.addStudent.setOnClickListener {
+            val id = binding.editStudentId.text.toString().toInt()
+            val name = binding.editStudentName.text.toString()
+            if (id > 0 && name.isNotEmpty()) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    myDao.insertStudent(Student(id, name))
+                }
+            }
+
+            binding.editStudentId.text = null
+            binding.editStudentName.text = null
+        }
+
+        binding.queryStudent.setOnClickListener {
+            val name = binding.editStudentName.text.toString()
+            CoroutineScope(Dispatchers.IO).launch {
+
+                val results = myDao.getStudentByName(name)
+
+                if (results.isNotEmpty()) {
+                    val str = StringBuilder().apply {
+                        results.forEach { student ->
+                            append(student.id)
+                            append("-")
+                            append(student.name)
+                        }
+                    }
+                    withContext(Dispatchers.Main) {
+                        binding.textQueryStudent.text = str
+                    }
+//                    runOnUiThread {
+//                        binding.textQueryStudent.text = str
+//                    }
+                } else {
+                    withContext(Dispatchers.Main) {
+                        binding.textQueryStudent.text = ""
+                    }
+                }
+            }
+        }
+
+        binding.deleteStudent.setOnClickListener {
+            val id = binding.editStudentId.text.toString().toInt()
+            val name = binding.editStudentName.text.toString()
+            if (id > 0 && name.isNotEmpty()) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    myDao.deleteStudent(Student(id, name))
+                }
+            }
+
+            binding.editStudentId.text = null
+            binding.editStudentName.text = null
+        }
     }
 }
---
+++ Ex11_googleMap/app/src/main/java/com/nhkim/ex11_googlemap/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nhkim.ex11_googlemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
